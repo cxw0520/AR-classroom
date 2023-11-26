35c354269ccdb4fdaf46cd4de82532994d4945e6
cxw0520 <james30088@gmail.com>
2023-11-26 16:32:26 +0800

    Updates

--- precache-manifest.50894d8f50ec838155b8979da000da0a.js
+++ precache-manifest.50894d8f50ec838155b8979da000da0a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e5a6e6392816405667e8fc6edff943ba",
+    "revision": "3c7ced6321abc0f920012601474e19cf",
     "url": "/AR-classroom/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/AR-classroom/static/css/2.c73450e6.chunk.css"
   },
   {
-    "revision": "d0cbcd8896731f4fff3f",
+    "revision": "6a2b3e93125b1b66cfdc",
     "url": "/AR-classroom/static/css/main.b2c17acd.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/AR-classroom/static/js/2.72302695.chunk.js"
   },
   {
-    "revision": "d0cbcd8896731f4fff3f",
-    "url": "/AR-classroom/static/js/main.0728f6d4.chunk.js"
+    "revision": "6a2b3e93125b1b66cfdc",
+    "url": "/AR-classroom/static/js/main.67ebc066.chunk.js"
   },
   {
     "revision": "fe081c0ea752b125b09e",
