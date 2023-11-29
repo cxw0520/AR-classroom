9c4fcae80c2da9d118354fe0bda9e49afca9b4f6
cxw0520 <james30088@gmail.com>
2023-11-30 01:14:05 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Avatar-classroom/precache-manifest.e794758ca17699dbcc256397e6ee3417.js"
+  "/Avatar-classroom/precache-manifest.0dbee9d7f1db5602ed03b93316ad5418.js"
 );
 
 self.addEventListener('message', (event) => {
