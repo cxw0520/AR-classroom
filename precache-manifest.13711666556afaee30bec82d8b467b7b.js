977b4c1de56fcc759114a2be70cbd6039df1bbf2
cxw0520 <james30088@gmail.com>
2023-11-29 21:30:18 +0800

    Updates

--- precache-manifest.13711666556afaee30bec82d8b467b7b.js
+++ precache-manifest.13711666556afaee30bec82d8b467b7b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7852c0ee9996006ebb75fafe5a93cdc1",
+    "revision": "7e42f7f6fa2b7a58a7655e9d713bbe6d",
     "url": "/Avatar-classroom/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Avatar-classroom/static/css/2.b602c17c.chunk.css"
   },
   {
-    "revision": "586292914ec98e81bd89",
+    "revision": "468fda328c3239049404",
     "url": "/Avatar-classroom/static/css/main.453f21f9.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Avatar-classroom/static/js/2.54b075dc.chunk.js"
   },
   {
-    "revision": "586292914ec98e81bd89",
-    "url": "/Avatar-classroom/static/js/main.7a953561.chunk.js"
+    "revision": "468fda328c3239049404",
+    "url": "/Avatar-classroom/static/js/main.624bdc55.chunk.js"
   },
   {
     "revision": "50d60281ea0511fdc264",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Avatar-classroom/precache-manifest.b6c399579b9049c6785a65e09530a0c2.js"
+  "/Avatar-classroom/precache-manifest.13711666556afaee30bec82d8b467b7b.js"
 );
 
 self.addEventListener('message', (event) => {
